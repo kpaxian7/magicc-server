161bd71ff56728d5e5c4a5860f10be0e5c19fe9f
zelong <smxldm@163.com>
2025-12-13 00:50:14 +0800

    modify：close auto reload

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from api import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=21200, debug=True, auto_reload=True)
+    app.run(host='0.0.0.0', port=21200, debug=True, auto_reload=False)
